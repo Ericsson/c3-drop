97acac6e69c3987b0791ce7e28dff0c237e524ea
Stefan Ålund <stefan.alund@ericsson.com>
2016-06-16 11:52:09 +0200

    License headers and screenshot

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
-function connect() {
+/*
+ * Copyright (C) 2016 Ericsson AB. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+ function connect() {
     var conn = document.getElementById('isConnected');
     conn.textContent = 'Connected';
     conn.style.color = 'rgba(0,255,0,0.5)';
