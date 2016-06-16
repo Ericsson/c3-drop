2c92849ee77117dba51020a54f5625024bc54048
Stefan Ålund <stefan.alund@ericsson.com>
2016-06-16 18:24:58 +0200

    Added touch device detector

--- app.js
+++ app.js
@@ -47,6 +47,10 @@ function showError() {
     $('.box__note').hide();
 }
 
+function isTouchDevice() {
+    return (('ontouchstart' in window) || (navigator.MaxTouchPoints > 0) || (navigator.msMaxTouchPoints > 0));
+}
+
 $(document).ready(function() {
 
     var isAdvancedUpload = function() {
