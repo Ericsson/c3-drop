81a0a347a0875c4c3eeffddd6bc761c517a54f70
Patrik Oldsberg <patrik.oldsberg@ericsson.com>
2017-04-02 09:50:05 +0200

    webpack: differ between production and dev build + add uglify plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,39 @@
 'use strict'
 
 const path = require('path')
+const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
+const isProd = process.env.NODE_ENV === 'production'
+const isDev = !isProd
+
+// Plugins
+
+const plugins = []
+
+plugins.push(new webpack.NamedModulesPlugin())
+plugins.push(new HtmlWebpackPlugin({title: 'Ericsson C3 Drop', hash: true}))
+
+if (isProd) {
+  plugins.push(new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      screw_ie8: true,
+      warnings: false,
+    },
+    mangle: {
+      screw_ie8: true,
+    },
+    output: {
+      comments: false,
+      screw_ie8: true,
+    },
+  }))
+}
+
+// Config
+
 module.exports = {
+  devtool: isDev ? 'eval-cheap-module-source-map' : false,
   context: __dirname,
   entry: path.join(__dirname, 'src'),
   output: {
@@ -11,10 +41,5 @@ module.exports = {
     publicPath: '',
     filename: 'bundle.js',
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Ericsson C3 Drop',
-      hash: true,
-    }),
-  ],
+  plugins,
 }
