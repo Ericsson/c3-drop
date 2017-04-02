55bfe9bd593147393e980f19c2d7f1bb10850a2f
Patrik Oldsberg <patrik.oldsberg@ericsson.com>
2017-04-02 09:55:21 +0200

    webpack: enable hot module replacement

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,10 @@ const plugins = []
 plugins.push(new webpack.NamedModulesPlugin())
 plugins.push(new HtmlWebpackPlugin({title: 'Ericsson C3 Drop', hash: true}))
 
+if (isDev) {
+  plugins.push(new webpack.HotModuleReplacementPlugin())
+}
+
 if (isProd) {
   plugins.push(new webpack.optimize.UglifyJsPlugin({
     compress: {
@@ -36,6 +40,9 @@ module.exports = {
   devtool: isDev ? 'eval-cheap-module-source-map' : false,
   context: __dirname,
   entry: path.join(__dirname, 'src'),
+  devServer: {
+    hot: true,
+  },
   output: {
     path: path.join(__dirname, 'dist'),
     publicPath: '',
